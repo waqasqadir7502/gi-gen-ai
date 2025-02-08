46f0e3790a3e6e4211d70ff56023bb7ef7f875c4
waqas qadir <65955447+waqasqadir7502@users.noreply.github.com>
2025-02-08 22:51:21 +0500

    2025 hackathon push

--- hackthons/ui-ux-hackathon-q2/app/layout.tsx
+++ hackthons/ui-ux-hackathon-q2/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import {Josefin_Sans} from "next/font/google"
 import {Lato} from "next/font/google"
 import "./globals.css";
